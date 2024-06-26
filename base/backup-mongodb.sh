8eb5b8c6c0733d35d7482a8da2cc2ed0987335d7
MPFabio <fabiomaratea@tutanota.com>
2024-06-26 22:07:38 +0200

    maj

--- base/backup-mongodb.sh
+++ base/backup-mongodb.sh
@@ -4,9 +4,4 @@ BACKUP_DIR=/data/db/backup-$TIMESTAMP
 mkdir -p $BACKUP_DIR
 mongodump --out $BACKUP_DIR
 
-# Upload to Azure Storage
-AZURE_STORAGE_ACCOUNT=$azurestorageaccountname
-AZURE_STORAGE_KEY=$azurestorageaccountkey
-AZURE_CONTAINER=$containername
-
 az storage blob upload-batch -d $AZURE_CONTAINER -s $BACKUP_DIR --account-name $AZURE_STORAGE_ACCOUNT --account-key $AZURE_STORAGE_KEY
